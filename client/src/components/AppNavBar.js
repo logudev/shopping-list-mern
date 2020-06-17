d32078d4cc561453e9ae1e0e0840527ced8c12e3
Logaraja D <logd@paypal.com>
2020-06-17 23:18:10 +0530

    Client side without redux

---
+++ client/src/components/AppNavBar.js
@@ -0,0 +1,38 @@
+import React, { Component, useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  Nav,
+  NavItem,
+  NavLink,
+  Container,
+  NavbarBrand,
+} from "reactstrap";
+
+export const AppNavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => {
+    console.log("Hamburger clicked");
+    setIsOpen(!isOpen);
+  };
+  return (
+    <div>
+      <Navbar color="dark" dark expand="sm" className="mb-5">
+        <Container>
+          <NavbarBrand>Shopping List</NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="ml-auto">
+              <NavItem>
+                <NavLink href="https://www.github.com/logudev" target="_blank">
+                  Github
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
