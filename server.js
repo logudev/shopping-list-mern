d7b69f1f6b465b12763142ce5101f0f03cbf209b
Logaraja D <logd@paypal.com>
2020-06-19 22:35:10 +0530

    Typo fix

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ connectToDB();
 app.use("/api/items", itemRoute);
 
 // Server static assets (build folder of client)
-if (process.env.NODE_ENV === production) {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
